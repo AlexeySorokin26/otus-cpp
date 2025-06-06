1f88901b3cab32cc393947c82ae31c84a7d68944
Alexey Sorokin <asorokin@gremse-it.com>
2025-06-06 17:48:55 +0300

    homework2 test

--- ip_filter_test.cpp
+++ ip_filter_test.cpp
@@ -2,10 +2,37 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "ip_filter.h"
+
 BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version) {
-	//BOOST_CHECK(version() > 0);
+	// test data
+	std::vector<std::vector<std::string>> test_ips = {
+		{"1", "1", "1", "1"},
+		{"1", "2", "3", "4"},
+		{"10", "20", "30", "40"},
+		{"1", "1", "1", "2"},
+		{"255", "255", "255", "255"},
+		{"192", "168", "1", "1"}
+	};
+
+	// expected data
+	std::vector<std::vector<std::string>> expected = {
+		{"255", "255", "255", "255"},
+		{"192", "168", "1", "1"},
+		{"10", "20", "30", "40"},
+		{"1", "2", "3", "4"},
+		{"1", "1", "1", "2"},
+		{"1", "1", "1", "1"}
+	};
+
+	reverseLexSort(test_ips);
+
+	BOOST_CHECK(test_ips.size() == expected.size());
+	for (size_t i = 0; i < test_ips.size(); ++i) {
+		BOOST_CHECK(test_ips[i] == expected[i]);
+	}
 }
 
 BOOST_AUTO_TEST_SUITE_END()
