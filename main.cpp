4e6decaea18300b0db090249a1cac6ab3c490ea5
Alexey Sorokin <asorokin@gremse-it.com>
2025-06-09 22:54:48 +0300

    homework4

--- main.cpp
+++ main.cpp
@@ -1,86 +1,47 @@
-#include <memory>
 #include <iostream>
-#include <map>
+#include <exception>
 #include <vector>
+#include <list>
+#include <cmath>
+#include <iomanip>
+#include <type_traits>
 
-template <typename T>
-class pool_allocator {
-public:
-    using value_type = T;
-
-    pool_allocator(size_t size = 10) : m_size(size), m_pool(new char[size * sizeof(T)]), m_used(size, false) {}
-
-    template <typename U>
-    pool_allocator(const pool_allocator<U>& other) : m_size(other.m_size), m_pool(new char[other.m_size * sizeof(T)]), m_used(other.m_size, false) {}
-
-    ~pool_allocator() {
-        delete[] static_cast<char*>(m_pool);
-    }
-
-    T* allocate(size_t n) {
-        if (n != 1) throw std::bad_alloc();
-
-        for (size_t i = 0; i < m_size; ++i) {
-            if (!m_used[i]) {
-                m_used[i] = true;
-                return reinterpret_cast<T*>(static_cast<char*>(m_pool) + i * sizeof(T));
-            }
-        }
-        throw std::bad_alloc();
-    }
-
-    void deallocate(T* p, size_t n) noexcept {
-        if (!p || n != 1) return;
-
-        size_t index = (reinterpret_cast<char*>(p) - static_cast<char*>(m_pool)) / sizeof(T);
-        if (index < m_size) {
-            m_used[index] = false;
-        }
-    }
-
-    template <typename U>
-    struct rebind {
-        using other = pool_allocator<U>;
-    };
-
-    template <typename U>
-    bool operator==(const pool_allocator<U>& other) const noexcept {
-        return this == &other;
-    }
-
-    template <typename U>
-    bool operator!=(const pool_allocator<U>& other) const noexcept {
-        return !(*this == other);
-    }
-
-private:
-    size_t m_size;
-    void* m_pool;
-    std::vector<bool> m_used;
-
-    template <typename U>
-    friend class pool_allocator;
-};
+template<typename T>
+void print(T&& val) {
+	if constexpr (std::is_same_v<std::remove_reference_t<T>, int>) {
+		do {
+			int tmp = val % 10;
+			std::cout << tmp << std::endl;
+			val = val / 10;
+		} while (val != 0);
+		std::cout << std::endl;
+	}
+	else if constexpr (std::is_same_v<std::remove_reference_t<T>, std::string>) {
+		std::cout << val << std::endl;
+	}
+	else if constexpr (std::is_same_v<std::remove_reference_t<T>, std::vector<typename std::remove_reference_t<T>::value_type>>) {
+		for (const auto& el : val) {
+			std::cout << el << ".";
+		}
+		std::cout << std::endl;
+	}
+	else {
+		static_assert(false, "Unsupported type");
+	}
+}
 
 int main() {
-    try {
-        pool_allocator<std::pair<const int, int>> alloc(20);
+	try {
+		print(12345);
+		std::vector<int>v{ 1, 2, 3, 4, 5 };
+		print(v);
+		std::string text{ "text" };
+		print(text);
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+		return 1;
+	}
 
-        std::map<int, int, std::less<int>,
-            pool_allocator<std::pair<const int, int>>> my_map(alloc);
-
-        for (int i = 0; i < 10; ++i) {
-            my_map[i] = i * i;
-        }
-
-        for (const auto& [key, value] : my_map) {
-            std::cout << key << ": " << value << "\n";
-        }
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-        return 1;
-    }
-
-    return 0;
+	return 0;
 }
